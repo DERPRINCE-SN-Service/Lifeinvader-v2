e2090f16fd3e5caa513407cab1dcfca8c6829f60
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-08-29 20:35:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,3 @@
---  ______  _   __        __     _  ____       ____                         __           
--- / ____/ / | / /       / /    (_)/ __/___   /  _/____  _   __ ____ _ ____/ /___   _____
--- \___ \ /  |/ /______ / /    / // /_ / _ \  / / / __ \| | / // __ `// __  // _ \ / ___/
--- ___/ // /|  //_____// /___ / // __//  __/_/ / / / / /| |/ // /_/ // /_/ //  __// /
---/____//_/ |_/       /_____//_//_/   \___//___//_/ /_/ |___/ \__,_/ \__,_/ \___//_/
-
---made by SN-Service https://discord.gg/snservice
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
@@ -12,25 +5,14 @@ author 'SN-Service'
 description "LifeInvader by DerPrince"
 version '1.2.0'
 
-escrow_ignore {
-    'config.lua',
-  }
-  
-  client_scripts {
-    'config.lua',
-  }
-  
-  
-  client_scripts {
-    'client.lua',
-  }
-  
-  
-  server_scripts {
-    'config.lua',
-  }
-  
-  server_scripts {
-    'server.lua',
-  }
+client_scripts {
+	'client.lua',
+	'config.lua',
+}
+
+server_scripts {
+	'server.lua',
+	'config.lua',
+}
+
 dependency '/assetpacks'
