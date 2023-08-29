11eeb2c8084c9b43ec52eda449bf1c9223580796
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-08-29 20:32:02 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,32 @@
+--  ______  _   __        __     _  ____       ____                         __           
+-- / ____/ / | / /       / /    (_)/ __/___   /  _/____  _   __ ____ _ ____/ /___   _____
+-- \___ \ /  |/ /______ / /    / // /_ / _ \  / / / __ \| | / // __ `// __  // _ \ / ___/
+-- ___/ // /|  //_____// /___ / // __//  __/_/ / / / / /| |/ // /_/ // /_/ //  __// /
+--/____//_/ |_/       /_____//_//_/   \___//___//_/ /_/ |___/ \__,_/ \__,_/ \___//_/
+
+--made by SN-Service https://discord.gg/snservice-io
+
+Config                            = {}
+
+----------Blip-Config----------
+Config.BlipCoords = {
+    {x=-1081.86, y=-248.59, z=36.80}
+}
+
+Config.BlipName = "LifeInvader"
+Config.BlipID = 77 --> https://docs.fivem.net/docs/game-references/blips/
+Config.BlipColour = 49 --> https://docs.fivem.net/docs/game-references/blips/
+Config.BlipActive = true
+
+Config.Time = 600 --> Zeit in Sekunden bis wieder Werbung geschaltet werden kann
+
+Config.Price = 500
+Config.Webhook = {
+    enable = true, --> true or false
+    link = "" --> paste your Discord Webhook here
+}
+
+Config.Discord = {
+    icon_url = "https://cdn.discordapp.com/attachments/1134454086180290561/1142171646849974393/Lifeinvader_icon.png", --> Icon Logo
+    thumb_url = "https://cdn.discordapp.com/attachments/1134454086180290561/1142171646849974393/Lifeinvader_icon.png", --> Thumbnail Logo
+}
